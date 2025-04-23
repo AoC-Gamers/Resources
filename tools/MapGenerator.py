703d36334bef916d5d0578364b8547ebc68c9d1d
lechuga <israel.la@hotmail.com>
2025-04-23 12:41:05 -0400

    Agregar MapGenerator.py y actualizar rutas en file-tree.json y redirects.json

--- tools/MapGenerator.py
+++ tools/MapGenerator.py
@@ -63,13 +63,149 @@ c13m3_memorialbridge,Cold Stream,Memorial Bridge,Oficial
 c13m4_cutthroatcreek,Cold Stream,Cut-throat Creek,Oficial
 c14m1_junkyard,The Last Stand,The Junkyard,Oficial
 c14m2_lighthouse,The Last Stand,Lighthouse Finale,Oficial
+l4d2_city17_01,City 17,1: Tunnels,WorkShop
+l4d2_city17_02,City 17,2: To the surface,WorkShop
+l4d2_city17_03,City 17,3: Streets,WorkShop
+l4d2_city17_04,City 17,4: Hospital,WorkShop
+l4d2_city17_05,City 17,5: Trainstation,WorkShop
+CCR1_alleys,Crash Course: ReRouted,Alleys,WorkShop
+CCR2_lots,Crash Course: ReRouted,Lots,WorkShop
+CCR3_Factories,Crash Course: ReRouted,Factories,WorkShop
+CCR4_Waterworks,Crash Course: ReRouted,Waterworks,WorkShop
+dkr_m1_motel,Dark Carnival Remix,The Motel,WorkShop
+dkr_m2_carnival,Dark Carnival Remix,The Carnival,WorkShop
+dkr_m3_tunneloflove,Dark Carnival Remix,The Tunnel of Love,WorkShop
+dkr_m4_ferris,Dark Carnival Remix,The Ferris Wheel,WorkShop
+dkr_m5_stadium,Dark Carnival Remix,The Stadium,WorkShop
+l4d2_daybreak01_hotel,Daybreak,1: The Hotel,WorkShop
+l4d2_daybreak02_coastline,Daybreak,2: The Coastline,WorkShop
+l4d2_daybreak03_bridge,Daybreak,3: The Bridge,WorkShop
+l4d2_daybreak04_cruise,Daybreak,4: The Outskirts,WorkShop
+l4d2_daybreak05_rescue,Daybreak,5: Alcatraz Finale,WorkShop
+l4d_dbd2dc_anna_is_gone,Dead Before Down,Anna is gone,WorkShop
+l4d_dbd2dc_the_mall,Dead Before Down,The mall,WorkShop
+l4d_dbd2dc_clean_up,Dead Before Down,Clean up,WorkShop
+l4d_dbd2dc_undead_center,Dead Before Down,Undead center,WorkShop
+l4d_dbd2dc_new_dawn,Dead Before Down,New dawn,WorkShop
+cdta_01detour,Detour Ahead,DTA Versus,WorkShop
+cdta_02road,Detour Ahead,DTA2 Versus,WorkShop
+cdta_03warehouse,Detour Ahead,DTA3 Versus,WorkShop
+cdta_04onarail,Detour Ahead,DTA4 Versus,WorkShop
+cdta_05finalroad,Detour Ahead,DTA5 Final Road,WorkShop
+l4d2_diescraper1_apartment_361,Diescraper Redux,Apartment complex,WorkShop
+l4d2_diescraper2_streets_361,Diescraper Redux,Downtown military outpost,WorkShop
+l4d2_diescraper3_mid_361,Diescraper Redux,The Skymall,WorkShop
+l4d2_diescraper4_top_361,Diescraper Redux,The top suites,WorkShop
+dprm1_milltown_a,Hard Rain Downpour,Chapter 1: Milltown,WorkShop
+dprm2_sugarmill_a,Hard Rain Downpour,Chapter 2: Sugarmill,WorkShop
+dprm3_sugarmill_b,Hard Rain Downpour,Chapter 3: Sugarmill Return,WorkShop
+dprm4_milltown_b,Hard Rain Downpour,Chapter 4: Milltown Return,WorkShop
+dprm5_milltown_escape,Hard Rain Downpour,Chapter 5: Milltown Escape,WorkShop
+l4d2_ff01_woods,Fatal Freight,1: The Woods,WorkShop
+l4d2_ff02_factory,Fatal Freight,2: The Factory,WorkShop
+l4d2_ff03_highway,Fatal Freight,3: The Highway,WorkShop
+l4d2_ff04_plant,Fatal Freight,4: The Plant,WorkShop
+l4d2_ff05_station,Fatal Freight,5: Trainyard Finale,WorkShop
+hf01_theforest,Haunted Forest,1: The Forest,WorkShop
+hf02_thesteeple,Haunted Forest,2: The Steeple,WorkShop
+hf03_themansion,Haunted Forest,3: The Mansion,WorkShop
+hf04_escape,Haunted Forest,4: The Tunnel,WorkShop
+l4d_ihm01_forest,I hate Mountains,1: Lost in the Woods,WorkShop
+l4d_ihm02_manor,I hate Mountains,2: Climbing the Manor,WorkShop
+l4d_ihm03_underground,I hate Mountains,3: The Underground Way,WorkShop
+l4d_ihm04_lumberyard,I hate Mountains,4: Lumberyard Evacuation,WorkShop
+l4d_ihm05_lakeside,I hate Mountains,5: Lakeside Finale,WorkShop
+nmrm1_apartment,No mercy Rehab,The Apartments,WorkShop
+nmrm2_subway,No mercy Rehab,The Subway,WorkShop
+nmrm3_sewers,No mercy Rehab,The Sewer,WorkShop
+nmrm4_hospital,No mercy Rehab,The Hospital,WorkShop
+nmrm5_rooftop,No mercy Rehab,Rooftop Finale,WorkShop
+x1m1_cliffs,Open Road,Cliffs,WorkShop
+x1m2_path,Open Road,Path,WorkShop
+x1m3_city,Open Road,City,WorkShop
+x1m4_forest,Open Road,Forest,WorkShop
+x1m5_salvation,Open Road,Salvation,WorkShop
+PR1_Waterfront_F,Parish OverGrowth,Waterfront,WorkShop
+PR2_Park_F,Parish OverGrowth,park,WorkShop
+PR3_Highway_F,Parish OverGrowth,Highway,WorkShop
+PR4_Quarter_F,Parish OverGrowth,Quarter,WorkShop
+PR5_Bridge_F,Parish OverGrowth,bridge,WorkShop
+l4d2_stadium1_apartment,Suicide Blitz 2,The Apartment Versus,WorkShop
+l4d2_vs_stadium2_riverwalk,Suicide Blitz 2,The Riverwalk Versus,WorkShop
+l4d2_stadium3_city1,Suicide Blitz 2,The Law Versus,WorkShop
+l4d2_stadium4_city2,Suicide Blitz 2,The City Versus,WorkShop
+l4d2_stadium5_stadium,Suicide Blitz 2,The Stadium Versus,WorkShop
+uz_crash,Undead Zone,Crash,WorkShop
+uz_town,Undead Zone,Town,WorkShop
+uz_desert,Undead Zone,Desert,WorkShop
+uz_bunker,Undead Zone,Bunker,WorkShop
+uz_escape,Undead Zone,Escape,WorkShop
+uf1_boulevard,Urban Flight,1: Boulevard (VS),WorkShop
+uf2_rooftops,Urban Flight,2: Rooftops (VS),WorkShop
+uf3_harbor,Urban Flight,3: Harbor (VS),WorkShop
+uf4_airfield,Urban Flight,4: Airfield (VS),WorkShop
+srocchurch,Warcelona,neighborhood,WorkShop
+plaza_espana,Warcelona,plaza,WorkShop
+maria_cristina,Warcelona,avenue,WorkShop
+mnac,Warcelona,gardens,WorkShop
+l4d_tbm_1,Bloody Moors,1: The Cottage,WorkShop
+l4d_tbm_2,Bloody Moors,2: Acid House Party,WorkShop
+l4d_tbm_3,Bloody Moors,3: 'Keep Off The Moors',WorkShop
+l4d_tbm_4,Bloody Moors,4: The Old Stag,WorkShop
+l4d_tbm_5,Bloody Moors,5: Arthur's Supplies,WorkShop
+cwm1_intro,Carried Off,The Riverbed,WorkShop
+cwm2_warehouse,Carried Off,The Town,WorkShop
+cwm3_drain,Carried Off,The Sewers,WorkShop
+cwm4_building,Carried Off,The Office Building,WorkShop
+c2m1_undarkv2,Unforgivable Night Redux,Port,WorkShop
+c2m2_undarkv2,Unforgivable Night Redux,Factory,WorkShop
+c2m3_undarkv2,Unforgivable Night Redux,Urban,WorkShop
+c2m4_undarkv2,Unforgivable Night Redux,Rooftop Construction,WorkShop
+dcr_m1_hotel,Dead Center: Rebirth,1: Hotel,WorkShop
+dcr_m2_streets,Dead Center: Rebirth,2: Streets,WorkShop
+dcr_m3_mall,Dead Center: Rebirth,3: Mall,WorkShop
+dcr_m4_atrium,Dead Center: Rebirth,4: Atrium,WorkShop
+DCR1_meetup_F,Dead Center: ReConstructed,meetup,WorkShop
+DCR2_TheVannha_F,Dead Center: ReConstructed,Vannha,WorkShop
+DCR3_Streets_F,Dead Center: ReConstructed,Streets,WorkShop
+DCR4_Mallentrance_F,Dead Center: ReConstructed,Mallentrance,WorkShop
+DCR5_Mall_F,Dead Center: ReConstructed,Mall,WorkShop
+l4d_deathaboard01_prison,Death Aboard II,Prison,WorkShop
+l4d_deathaboard02_yard,Death Aboard II,Prison Yard,WorkShop
+l4d_deathaboard03_docks,Death Aboard II,Docks,WorkShop
+l4d_deathaboard04_ship,Death Aboard II,Ship,WorkShop
+l4d_deathaboard05_light,Death Aboard II,Lighthouse,WorkShop
+dsr_m1,Death Sentence Redux,Satans Square,WorkShop
+dsr_m2,Death Sentence Redux,The Stink of Flesh,WorkShop
+dsr_m3,Death Sentence Redux,Hostil Campsite,WorkShop
+dsr_m4_sv,Death Sentence Redux,Village of the Damned,WorkShop
+dsr_m5,Death Sentence Redux,Slaughter Island,WorkShop
+fos1m1_hill,FOS1,fos1m1(VS),WorkShop
+fos1m2_fftown,FOS1,fos1m2(VS),WorkShop
+fos1m3_factory,FOS1,fos1m3(VS),WorkShop
+fos1m4_moors,FOS1,fos1m4(VS),WorkShop
+fos1m5_92farm,FOS1,fos1m5(VS),WorkShop
+noecho_m1,No Echo Match,noecho_m1 (VS),WorkShop
+noecho_m2,No Echo Match,noecho_m2 (VS),WorkShop
+noecho_m3,No Echo Match,noecho_m3 (VS),WorkShop
+noecho_m4,No Echo Match,noecho_m4 (VS),WorkShop
+noecho_m5,No Echo Match,noecho_m5 (VS),WorkShop
+outline_m1,Outline,Rise up,WorkShop
+outline_m2,Outline,Visit the city,WorkShop
+outline_m3,Outline,Through the town,WorkShop
+outline_m4,Outline,Rest in yard,WorkShop
+tripday_new_m1,Trip day Match,tripday:match (1/5),WorkShop
+tripday_new_m2,Trip day Match,tripday:match (2/5),WorkShop
+tripday_new_m3,Trip day Match,tripday:match (3/5),WorkShop
+tripday_new_m4,Trip day Match,tripday:match (4/5),WorkShop
+tripday_new_m5,Trip day Match,tripday:match (5/5),WorkShop
 """
 
 # ========= Config por argumentos ========= #
 debug = False
 onlyOficial = False
 onlyWorkShop = False
-cantidad_campañas = 15
+cantidad_campañas = 50
 
 # ========= Funciones ========== #
 def generar_maplist_csv(filename):
@@ -248,6 +384,23 @@ def main():
     csv_file = "Maplist.csv"
     if not os.path.exists(csv_file):
         generar_maplist_csv(csv_file)
+
+    # Cargar el CSV para validaciones
+    try:
+        with open(csv_file, 'r', encoding='utf-8-sig') as f:
+            reader = csv.DictReader(f)
+            datos_csv = list(reader)
+    except Exception as e:
+        print(f"❌ Error leyendo CSV para verificación: {e}")
+        return
+
+    if onlyOficial and not any(row.get("Origen", "").strip() == "Oficial" for row in datos_csv):
+        print("❌ No se encontraron mapas oficiales en el archivo CSV.")
+        return
+
+    if onlyWorkShop and not any(row.get("Origen", "").strip() == "WorkShop" for row in datos_csv):
+        print("❌ No se encontraron mapas de Workshop en el archivo CSV.")
+        return
 
     grupos_json = "Grupos.json"
     campañas_json = "Campañas.json"
